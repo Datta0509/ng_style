653cd5f541bcd9288cc15f21a58b30dbd1e8034f
Datta0509 <datta.pandya.23@gmail.com>
2023-04-17 10:04:01 +0530

    added component if-else condition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { IelseComponent } from './ielse/ielse.component';
+import { IfElseConditionComponent } from './if-else-condition/if-else-condition.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    IelseComponent
+    IfElseConditionComponent
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/if-else-condition/if-else-condition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-if-else-condition',
+  templateUrl: './if-else-condition.component.html',
+  styleUrls: ['./if-else-condition.component.css']
+})
+export class IfElseConditionComponent {
+
+}
